201da6c216ccf964a819fc8d951d5f96e17ee04e
hy <denise13@163.com>
2023-04-07 03:59:00 -0400

    add out for project
    Signed-off-by: hy <denise13@163.com>

--- src/demo_so/hello_so.cc
+++ src/demo_so/hello_so.cc
@@ -1,11 +1,14 @@
+#include <iostream>
 #include <string>
 #include "./hello_so.h"
 
+
 using namespace std;
+
 
 int say_hello()
 {
-    std::string = "hello";
-    std::out << string << std::endl;
+    std::string hello("hello");
+    std::cout << hello << std::endl;
     return 0;
 } 
--- src/demo_so/hello_so.h
+++ src/demo_so/hello_so.h
@@ -1,8 +1,9 @@
 #ifndef __HELLO_SO_H__
 #define __HELLO_SO_H__
 
-#include <string>
 
-using namesapce std;
 
 int say_hello();
+
+#endif
+
